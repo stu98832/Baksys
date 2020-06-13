755931062e4edd91189d48a86d26de48f142f1d6
stu98832 <maplestory988323@gmail.com>
2020-06-13 14:26:33 +0800

    Rewrite functions' name in app.server.client(delete underline prefix)

--- app/server/client.py
+++ app/server/client.py
@@ -7,24 +7,24 @@ class BaksysUser:
         this.login               = False
         this.username            = ''
         this.socket              = socket
-        this.socket.onReceive    = this._handleMessage
-        this.socket.onDisconnect = this._onDisconnect
+        this.socket.onReceive    = this.handleMessage
+        this.socket.onDisconnect = this.onDisconnect
         this.onDisconnect        = lambda s: None
         this.socket.startReceive()
         
     def __del__(this):
         this.socket.close()
         
-    def _onDisconnect(this, socket):
+    def onDisconnect(this, socket):
         this.socket.close()
         this.onDisconnect(this)
         
-    def _checkLogin(this):
+    def checkLogin(this):
         if not this.login:
             this.socket.disconnect()
         return this.login
         
-    def _handleMessage(this, socket, message):
+    def handleMessage(this, socket, message):
         header = message.readByte()
         if   header == packet.REQUEST_LOGIN:
             username = message.readString()
@@ -32,15 +32,15 @@ class BaksysUser:
             handler.handleLogin(this, username, password)
             
         elif header == packet.REQUEST_BACKUP:
-            if this._checkLogin():
+            if this.checkLogin():
                 handler.handleBackupRequest(this, message)
                 
         elif header == packet.REQUEST_UPLOAD:
-            if this._checkLogin():
+            if this.checkLogin():
                 handler.handleUploadRequest(this, message)
                 
         elif header == packet.REQUEST_DOWNLOAD:
-            if this._checkLogin():
+            if this.checkLogin():
                 handler.handleDownloadRequest(this, message)
             
         else:
