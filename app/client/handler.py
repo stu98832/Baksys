be728c31e1f544113760629bac2e3161ca3052fa
stu98832 <maplestory988323@gmail.com>
2020-06-13 10:46:19 +0800

    Move process block of packet from app.client.remote_backup.onPacket to app.client.handler

---
+++ app/client/handler.py
@@ -0,0 +1,78 @@
+import os
+import shutil
+from   baksys.event          import *
+import app.com.packet        as packet
+import app.com.packet.backup
+
+def handleBackup(remoteBackup, message):
+    subtype = message.readByte()
+    if subtype == packet.backup.REQUEST_TYPE_LIST:
+        count = message.readEncodingInt()
+        backupList = []
+        for i in range(count):
+            path    = message.readString()
+            orgpath = message.readString()
+            size    = message.readLong()
+            crc     = message.readUInt()
+            backupList.append({                         \
+                'name'        : os.path.basename(path), \
+                'path'        : path,                   \
+                'origin_path' : orgpath,                \
+                'size'        : size,                   \
+                'crc'         : crc,                    \
+            })
+        remoteBackup.setResponse(backupList)
+    # end REQUEST_TYPE_LIST
+    elif subtype == packet.backup.REQUEST_TYPE_UPDATE_LIST:
+        count = message.readEncodingInt()
+        updateList = []
+        for i in range(count):
+            updateList.append(message.readString())
+        remoteBackup.setResponse(updateList)
+    # end REQUEST_TYPE_UPDATE_LIST
+# end handleBackup
+
+def handleDownload(remoteBackup, message):
+    subtype = message.readByte()
+    if   subtype == packet.backup.UPLOAD_DOWNLOAD_CONTINUE:
+        try:
+            if not remoteBackup.downloadFile:
+                remoteBackup.downloadInterrupt()
+                return
+            offset = message.readLong()
+            size   = message.readLong()
+            data   = message.readBuffer(size)
+            remoteBackup.downloadFile['offset'] = offset
+            remoteBackup.downloadFile['size']  += size
+            remoteBackup.downloadFile['file'].seek(offset, 0)
+            remoteBackup.downloadFile['file'].write(data)
+            
+            # event invoke
+            args = BaksysEventArgs()
+            args.progress = remoteBackup.downloadFile['size'] / remoteBackup.downloadFile['total_size']
+            remoteBackup.onDownloadProcess.invoke(args)
+        except Exception as e:
+            remoteBackup.downloadInterrupt()
+            remoteBackup.lastOperationError = str(e)
+            remoteBackup.setResponse(False)
+    # end UPLOAD_DOWNLOAD_CONTINUE
+    elif subtype == packet.backup.UPLOAD_DOWNLOAD_FINISH:
+        if not remoteBackup.downloadFile:
+            remoteBackup.downloadInterrupt()
+            return
+        remoteBackup.downloadFile['file'].close()
+        if os.path.exists(remoteBackup.downloadFile['path']):
+            os.remove(remoteBackup.downloadFile['path'])
+        shutil.move(remoteBackup.downloadFile['temp'], remoteBackup.downloadFile['path'])
+        remoteBackup.downloadFile = { }
+        remoteBackup.setResponse(True)
+    # end UPLOAD_DOWNLOAD_FINISH
+    elif subtype == packet.backup.UPLOAD_DOWNLOAD_BREAK:
+        remoteBackup.lastOperationError = 'operation has been interrupted by remote server'
+        remoteBackup.setResponse(False)
+    # end UPLOAD_DOWNLOAD_BREAK
+    elif subtype == packet.backup.UPLOAD_DOWNLOAD_START:
+        remoteBackup.downloadFile['total_size'] = message.readLong()
+        remoteBackup.setResponse(True)
+    # end UPLOAD_DOWNLOAD_START
+# end handleDownload
--- app/client/remote_backup.py
+++ app/client/remote_backup.py
@@ -1,20 +1,21 @@
 import os
-import shutil
+import time
 import threading
 from   random              import random
 from   baksys.net          import *
 from   baksys.event        import *
 from   app.client.setting  import *
+import app.client.handler    as handler
 import app.com.packet        as packet
-import app.com.packet.login  as loginPacket
-import app.com.packet.backup as backupPacket
+import app.com.packet.login 
+import app.com.packet.backup 
 
 class BaksysRemoteBackup:
     def __init__(this):
         this.socket             = BaksysClientSocket()
         this.responseLock       = threading.Event()
         this.remoteResponse     = { }
-        this.fileOperation      = { }
+        this.downloadFile       = { }
         this.lastOperationError = ''
         this.onDownloadProcess  = BaksysEvent()
         
@@ -27,8 +28,8 @@ class BaksysRemoteBackup:
             
         try:
             this.socket.connect(address, port)
-            this.socket.onReceive    = this._onPacket
-            this.socket.onDisconnect = this._onConnectReset
+            this.socket.onReceive    = this.onPacket
+            this.socket.onDisconnect = this.onConnectReset
             this.socket.startReceive()
         except Exception as e:
             return False
@@ -38,66 +39,29 @@ class BaksysRemoteBackup:
         this.socket.disconnect()
         
     def login(this, username, password):
-        this.socket.send(loginPacket.loginRequest(username, password))
-        return this._waitForResponse()
+        this.socket.send(packet.login.loginRequest(username, password))
+        return this.waitForResponse()
         
     def getUpdateList(this, localList):
-        this.socket.send(backupPacket.updateListRequest(localList))
-        return this._waitForResponse()
+        this.socket.send(packet.backup.updateListRequest(localList))
+        return this.waitForResponse()
         
     def getList(this):
-        this.socket.send(backupPacket.listRequest())
-        return this._waitForResponse()
+        this.socket.send(packet.backup.listRequest())
+        return this.waitForResponse()
         
     def downloadInterrupt(this):
-        this.socket.send(backupPacket.downloadBreakRequest())
-        
-    def handleDownload(this, message):
-        subtype = message.readByte()
-        if   subtype == backupPacket.UPLOAD_DOWNLOAD_CONTINUE:
-            try:
-                if not this.fileOperation:
-                    this.downloadInterrupt()
-                    return
-                offset = message.readLong()
-                size   = message.readLong()
-                data   = message.readBuffer(size)
-                this.fileOperation['offset'] = offset
-                this.fileOperation['size']  += size
-                this.fileOperation['file'].seek(offset, 0)
-                this.fileOperation['file'].write(data)
-                
-                # event invoke
-                args = BaksysEventArgs()
-                args.progress = this.fileOperation['size'] / this.fileOperation['total_size']
-                this.onDownloadProcess.invoke(args)
-            except Exception as e:
-                this.downloadInterrupt()
-                this.lastOperationError = str(e)
-                this._setResponse(False)
-                
-        elif subtype == backupPacket.UPLOAD_DOWNLOAD_FINISH:
-            if not this.fileOperation:
-                this.downloadInterrupt()
-                return
-            this.fileOperation['file'].close()
-            shutil.move(this.fileOperation['temp'], this.fileOperation['path'])
-            this.fileOperation = {}
-            this._setResponse(True)
-            
-        elif subtype == backupPacket.UPLOAD_DOWNLOAD_BREAK:
-            this.lastOperationError = 'operation has been interrupted by remote server'
-            this._setResponse(False)
-            
-        elif subtype == backupPacket.UPLOAD_DOWNLOAD_START:
-            this.fileOperation['total_size'] = message.readLong()
-            this._setResponse(True)
+        this.socket.send(packet.backup.downloadBreakRequest())
+        if this.downloadFile['file']:
+            this.downloadFile['file'].close()
+            os.remove(this.downloadFile['temp'])
+        this.downloadFile = { }
             
     def downloadRequest(this, path):
         temppath = ''
         while temppath == '' or os.path.exists(temppath):
             temppath = os.path.join(BAKSYS_TEMPDIR, 'download_%08X'%int(random()*0x100000000))
-        this.fileOperation = {                                        \
+        this.downloadFile = {                                         \
             'path'       : os.path.join(config['backup_path'], path), \
             'temp'       : temppath,                                  \
             'file'       : None,                                      \
@@ -107,87 +71,69 @@ class BaksysRemoteBackup:
         }
         if not os.path.exists(BAKSYS_TEMPDIR):
             os.makedirs(BAKSYS_TEMPDIR)
-        this.fileOperation['file'] = open(this.fileOperation['temp'], 'wb')
-        this.socket.send(backupPacket.downloadRequest(path))
-        return this._waitForResponse()
+        this.downloadFile['file'] = open(this.downloadFile['temp'], 'wb')
+        this.socket.send(packet.backup.downloadRequest(path))
+        return this.waitForResponse()
+    # end downloadRequest
             
     def downloadBackup(this):
-        if not this.fileOperation:
-            this.lastOperationError = 'not any download request.'
+        if not this.downloadFile:
+            this.lastOperationError = 'no avaiable download request.'
             return False
-        return this._waitForResponse()
+        return this.waitForResponse()
         
     def uploadBackup(this, backup, path):
         # TODO
         pass
         
     def deleteBackup(this, name):
-        this.socket.send(backupPacket.deleteRequest(name))
-        return this._waitForResponse()
+        this.socket.send(packet.backup.deleteRequest(name))
+        return this.waitForResponse()
             
-    def _waitForResponse(this):
+    def waitForResponse(this):
         this.remoteResponse = {\
             'result'   : None, \
             'error'    : None
         }
         this.responseLock.clear()
-        this.responseLock.wait()
+        while not this.responseLock.is_set():
+            time.sleep(0.1)
         if this.remoteResponse['error']:
             raise RuntimeError(this.remoteResponse['error'])
         result =  this.remoteResponse['result']
         this.remoteResponse = { }
         return result
+    # end waitForResponse
     
-    def _onConnectReset(this, socket):
-        this._setResponseError('disconnect from server')
+    def onConnectReset(this, socket):
+        this.setResponseError('disconnect from server')
             
-    def _setResponseError(this, message):
+    def setResponseError(this, message):
         if this.remoteResponse:
             this.remoteResponse['error'] = message
             this.responseLock.set()
             
-    def _setResponse(this, result):
+    def setResponse(this, result):
         if this.remoteResponse:
             this.remoteResponse['result'] = result
             this.responseLock.set()
         
-    def _onPacket(this, socket, message):
+    def onPacket(this, socket, message):
         header = message.readByte()
         
         if   header == packet.RESPONSE_OPERATION:
             success = (message.readByte() == 1)
             if not success:
                 this.lastOperationError = message.readString()
-            this._setResponse(success)
+            this.setResponse(success)
             
         elif header == packet.RESPONSE_BACKUP:
-            subtype = message.readByte()
-            if subtype == backupPacket.REQUEST_TYPE_LIST:
-                count = message.readEncodingInt()
-                backupList = []
-                for i in range(count):
-                    path    = message.readString()
-                    orgpath = message.readString()
-                    size    = message.readLong()
-                    crc     = message.readUInt()
-                    backupList.append({                         \
-                        'name'        : os.path.basename(path), \
-                        'path'        : path,                   \
-                        'origin_path' : orgpath,                \
-                        'size'        : size,                   \
-                        'crc'         : crc,                    \
-                    })
-                this._setResponse(backupList)
-            elif subtype == backupPacket.REQUEST_TYPE_UPDATE_LIST:
-                count = message.readEncodingInt()
-                updateList = []
-                for i in range(count):
-                    updateList.append(message.readString())
-                this._setResponse(updateList)
+            handler.handleBackup(this, message)
             
         elif header == packet.RESPONSE_DOWNLOAD:
-            this.handleDownload(message)
+            handler.handleDownload(this, message)
             
         else:
             # TODO: process invalid packet
             pass
+    # end onPacket
